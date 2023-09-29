64bfb9c4434c7f8591df684752b2bd19563f5a0f
Daniel Slosky <daniel.slosky@caylent.com>
2023-09-29 11:58:09 -0600

    Performance testing

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-
-This simple library is an API performance analytics tool. It's sole purpose is to collect execution times for specific test cases, which are defined in the code.
+This simple library is an API performance analytics tool. It's sole purpose is to collect execution times for specific test cases, which are defined in the code. This module will be useful when benchmark testing an application's API for a user, or when researching technologies to determine which is the most appropriate for a specific solution. It was originally generated to benchmark test a FastAPI application against a custom-code API solution. I would love to see it grow and mature if there are other types of benchmark testing or test conditions that would make this library more useful.
 
 # Setup
 
@@ -8,6 +7,8 @@ source ./.venv/bin/activate
 pip3 install -r requirements.txt
 
 # Define your test cases
+
+Define your test cases in the `./bin/benchmark.py` module.
 
 Tests are stored in the variable `tests = []`
 The base URL for your tests is saved in `BASE_URL = ""`
@@ -24,9 +25,17 @@ Test case definitions use the following schema:
     "expected_code": 201, # The response code we're expecting
     "data": {
         "name": "test", # this data will be passed with the request. If the METHOD is a POST
-        "number": "a"   # it will be send in the request body; for a GET they will be passed as URL parameters
-    },
+        "number": "a"   # it will be send in the request body; for a GET they will be passed 
+    },                  # as query parameters
     "repetitions": 50 # how many times this test should be repeated
 }
 
 ```
+
+# Running the module
+
+`python3 ./bin/benchmark.py`
+
+# Outputs
+
+The module currently only supports text generation outputs. Each test case generates its own text file (using the test `name` parameter) which is simply a list of execution times in miliseconds.
---
+++ bin/benchmark.py
@@ -0,0 +1,83 @@
+#! /usr/bin/python3
+
+import time
+
+import requests
+
+
+BASE_URL = "https://mv9f04rfy9.execute-api.us-east-1.amazonaws.com/v1"
+FILE_SUFFIX = "-benchmarks.txt"
+
+tests = [
+    # {
+    #     "name": "custom-warm-start-GET-success",
+    #     "sleep": .1,
+    #     "url": "/custom",
+    #     "method": "GET",
+    #     "expected_code": 200,
+    #     "data": {
+    #         "search": "test"
+    #     },
+    #     "repetitions": 51
+    # },
+    # {
+    #     "name": "custom-cold-start-POST-success",
+    #     "sleep": 360,
+    #     "url": "/custom",
+    #     "method": "POST",
+    #     "expected_code": 201,
+    #     "data": {
+    #         "name": "test",
+    #         "number": 1
+    #     },
+    #     "repetitions": 51
+    # },
+]
+
+def write_output(name, data):
+    data_str = "\n".join(data)
+    with open(f"{name}-{FILE_SUFFIX}", "w") as file_:
+        file_.write(data_str)
+
+def send_post_request(test_case):
+    return requests.post(
+        f"{BASE_URL}{test_case['url']}", json=test_case["data"]
+    )
+
+def send_get_request(test_case):
+    url = f"{BASE_URL}{test_case['url']}"
+    return requests.get(url, test_case["data"])
+
+def execute_test(test_case):
+    if test_case["method"] == "GET":
+        return send_get_request(test_case)
+    elif test_case["method"] == "POST":
+        return send_post_request(test_case)
+
+def main():
+    for test in tests:
+        data = []
+        print(test["name"])
+        for i in range(test["repetitions"]):
+            time.sleep(test["sleep"])
+
+            t_start = time.time()
+            result = execute_test(test)
+            t_finish = time.time()
+
+            if result.status_code != test["expected_code"]:
+                raise Exception("REQUEST FAILED for test: ", test, f"\nGot status code: {result.status_code}")
+
+            time_result = int((t_finish-t_start) * 1000)
+            data.append(str(time_result))
+            print(time_result)
+
+        write_output(test["name"], data)
+        
+
+if __name__ == "__main__":
+    main()
+
+
+
+
